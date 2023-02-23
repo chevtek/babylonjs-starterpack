bae853aa1ea893b20d0bad91552f6dd29a4611bb
Chev <chev@chevtek.io>
2023-02-23 15:06:04 -0700

    Adding prettier code formatting.

--- src/BabylonWorkspace.tsx
+++ src/BabylonWorkspace.tsx
@@ -10,13 +10,12 @@ import {
   MeshBuilder,
   Scene,
   StandardMaterial,
-  Vector3
+  Vector3,
 } from "@babylonjs/core";
 import store from "./store";
 import { changeColor } from "./store/slices/appSlice";
 
 function BabylonWorkspace() {
-
   const canvasRef = useCallback((canvas: HTMLCanvasElement) => {
     if (!canvas) return;
 
@@ -24,16 +23,38 @@ function BabylonWorkspace() {
     const engine = new Engine(canvas);
     const scene = new Scene(engine);
     new AxesViewer(scene);
-    const mainCamera = new ArcRotateCamera("mainCamera", 0, Math.PI / 3, 10, Vector3.Zero(), scene);
-    const hudCamera = new ArcRotateCamera("hudCamera", 0, Math.PI / 3, 10, Vector3.Zero(), scene);
+    const mainCamera = new ArcRotateCamera(
+      "mainCamera",
+      0,
+      Math.PI / 3,
+      10,
+      Vector3.Zero(),
+      scene
+    );
+    const hudCamera = new ArcRotateCamera(
+      "hudCamera",
+      0,
+      Math.PI / 3,
+      10,
+      Vector3.Zero(),
+      scene
+    );
     mainCamera.onViewMatrixChangedObservable.add(() => {
       hudCamera.alpha = mainCamera.alpha;
       hudCamera.beta = mainCamera.beta;
     });
     mainCamera.attachControl(canvas, true);
-    const light = new HemisphericLight("light", new Vector3(0.5, 1, 0.25), scene);
+    const light = new HemisphericLight(
+      "light",
+      new Vector3(0.5, 1, 0.25),
+      scene
+    );
     light.intensity = 0.75;
-    const ground = MeshBuilder.CreateGround("ground", { width: 10, height: 10 }, scene);
+    const ground = MeshBuilder.CreateGround(
+      "ground",
+      { width: 10, height: 10 },
+      scene
+    );
     const groundMaterial = new StandardMaterial("ground", scene);
     groundMaterial.diffuseColor = Color3.Gray();
     ground.material = groundMaterial;
@@ -48,24 +69,28 @@ function BabylonWorkspace() {
     box.material = boxMaterial;
 
     box.actionManager = new ActionManager(scene);
-    box.actionManager.registerAction(new ExecuteCodeAction(ActionManager.OnPickTrigger, evt => {
-      store.dispatch(changeColor(Color3.Yellow()));
-    }));
+    box.actionManager.registerAction(
+      new ExecuteCodeAction(ActionManager.OnPickTrigger, (evt) => {
+        store.dispatch(changeColor(Color3.Yellow()));
+      })
+    );
 
     window.onresize = () => {
       engine.resize();
-    }
+    };
 
     engine.runRenderLoop(() => {
       scene.render();
     });
   }, []);
 
-  return <canvas
-    style={{ height: "100%", width: "100%" }}
-    ref={canvasRef}
-    id="babylonWorkspace"
-  />;
+  return (
+    <canvas
+      style={{ height: "100%", width: "100%" }}
+      ref={canvasRef}
+      id="babylonWorkspace"
+    />
+  );
 }
 
 export default BabylonWorkspace;
